06bf641ae85f83e2a11961b8917b6ac6fba8ca7d
hyun6ik <fkanwm@naver.com>
2024-01-02 20:33:09 +0900

    feat : Common Custom Annotation 생성

---
+++ membership-service/src/main/kotlin/com/hyun6ik/common/UseCase.kt
@@ -0,0 +1,17 @@
+package com.hyun6ik.common
+
+import org.springframework.core.annotation.AliasFor
+import org.springframework.stereotype.Component
+
+@Target(AnnotationTarget.CLASS)
+@Retention(AnnotationRetention.RUNTIME)
+@MustBeDocumented
+@Component
+annotation class UseCase(
+
+    @get:AliasFor(annotation = Component::class)
+    val value: String = ""
+){
+
+
+}
--- membership-service/src/main/kotlin/com/hyun6ik/membershipservice/adapter/in/web/RegisterMembershipController.kt
+++ membership-service/src/main/kotlin/com/hyun6ik/membershipservice/adapter/in/web/RegisterMembershipController.kt
@@ -1,10 +1,12 @@
 package com.hyun6ik.membershipservice.adapter.`in`.web
 
+import com.hyun6ik.common.WebAdapter
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
+@WebAdapter
 @RestController
-class TestController {
+class RegisterMembershipController {
 
     @GetMapping("ping")
     fun pong() = "pong"
